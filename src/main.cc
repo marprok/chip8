154b56e2a0935410f8f642e23a461f57d688db4e
Marios Prokopakis <mariosprokopakis@gmail.com>
2025-02-15 15:55:04 +0200

    Factor out the display code into a new class

--- src/main.cc
+++ src/main.cc
@@ -12,6 +12,102 @@ static constexpr std::size_t  MAX_ADDR = 0x1000;
 static constexpr std::int32_t WIDTH    = 64;
 static constexpr std::int32_t HEIGHT   = 32;
 
+template <std::int32_t W = 64, std::int32_t H = 32>
+struct Display
+{
+    ~Display()
+    {
+        /* Frees memory */
+        SDL_FreeSurface(m_surf);
+        SDL_DestroyTexture(m_ttex);
+        SDL_DestroyRenderer(m_rend);
+        SDL_DestroyWindow(m_win);
+    }
+
+    bool init() noexcept
+    {
+        m_win = SDL_CreateWindow("CHIP-8",                /* Title of the SDL window */
+                                 SDL_WINDOWPOS_UNDEFINED, /* Position x of the window */
+                                 SDL_WINDOWPOS_UNDEFINED, /* Position y of the window */
+                                 W,                       /* Width of the window in pixels */
+                                 H,                       /* Height of the window in pixels */
+                                 0);                      /* Additional flag(s) */
+        if (!m_win)
+        {
+            std::cerr << "SDL window failed to initialise: " << SDL_GetError() << '\n';
+            return false;
+        }
+        // TODO: frame cap flag??
+        m_rend = SDL_CreateRenderer(m_win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE | SDL_RENDERER_PRESENTVSYNC);
+        if (m_rend == NULL)
+        {
+            std::cerr << "SDL renderer failed to initialise: " << SDL_GetError() << '\n';
+            return false;
+        }
+        // TODO: endianes check
+        m_surf = SDL_CreateRGBSurfaceFrom(m_memory.data(), W, H, 32, 4 * W, 0xFF000000, 0x00FF0000, 0x0000FF00, 0x000000FF);
+        if (m_surf == NULL)
+        {
+            std::cerr << "SDL surface cration failed: " << SDL_GetError() << '\n';
+            return false;
+        }
+
+        m_ttex = SDL_CreateTexture(m_rend, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, WIDTH, HEIGHT);
+        if (m_ttex == NULL)
+        {
+            std::cerr << "SDL could not create texturetarget: " << SDL_GetError() << '\n';
+            return false;
+        }
+
+        return true;
+    }
+
+    void set_pixels(std::int32_t x, std::int32_t y, std::uint8_t sprite_row)
+    {
+        for (std::uint8_t i = 0; i < 8u && (x + i) < W; ++i)
+        {
+            m_memory[y * W * 4 + (x + i) * 4 + 2] = m_memory[y * W * 4 + (x + i) * 4 + 1] = m_memory[y * W * 4 + (x + i) * 4] ^= ((sprite_row >> (7 - i)) & 0x1) * 255;
+        }
+    }
+
+    void draw()
+    {
+        SDL_Texture* tex = SDL_CreateTextureFromSurface(m_rend, m_surf);
+        if (tex == NULL)
+        {
+            std::cerr << "SDL could not create temp texture: " << SDL_GetError() << '\n';
+            return;
+        }
+        SDL_RenderClear(m_rend);
+
+        SDL_SetRenderTarget(m_rend, m_ttex);
+        SDL_RenderClear(m_rend);
+        SDL_RenderCopy(m_rend, tex, NULL, NULL);
+
+        SDL_SetRenderTarget(m_rend, NULL);
+        SDL_RenderCopy(m_rend, m_ttex, NULL, NULL);
+        SDL_RenderPresent(m_rend);
+        SDL_DestroyTexture(tex);
+    }
+
+    inline void reset(std::uint8_t value)
+    {
+        for (std::int32_t i = 0; i < W * H; i += 4)
+        {
+            m_memory[i] = m_memory[i + 1] = m_memory[i + 2] = value;
+            m_memory[i + 3]                                 = 0xFF;
+        }
+    }
+
+private:
+    SDL_Window*   m_win {};
+    SDL_Renderer* m_rend {};
+    SDL_Surface*  m_surf {};
+    SDL_Texture*  m_ttex {}; // target texture
+
+    std::array<std::uint8_t, W * 4 * H> m_memory {};
+};
+
 std::uint16_t read_program(const fs::path& program_file, std::array<std::uint8_t, MAX_ADDR>& RAM, const std::uint16_t base_addr = 0x200)
 {
     std::fstream in { program_file, in.binary | in.in };
@@ -31,66 +127,17 @@ inline void TODO(const std::string_view instruction)
     std::cout << "TODO: " << instruction << '\n';
 }
 
-int main(int argc, char** argv)
+int run()
 {
-    static_cast<void>(argc);
-    static_cast<void>(argv);
-
-    /* Initialises data */
-    SDL_Window* window = NULL;
-
-    /*
-     * Initialises the SDL video subsystem (as well as the events subsystem).
-     * Returns 0 on success or a negative error code on failure using SDL_GetError().
-     */
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
-    {
-        std::cerr << "SDL failed to initialise: " << SDL_GetError() << '\n';
+    Display display;
+    if (!display.init())
         return 1;
-    }
-
-    /* Creates a SDL window */
-    window = SDL_CreateWindow("SDL Example",           /* Title of the SDL window */
-                              SDL_WINDOWPOS_UNDEFINED, /* Position x of the window */
-                              SDL_WINDOWPOS_UNDEFINED, /* Position y of the window */
-                              WIDTH,                   /* Width of the window in pixels */
-                              HEIGHT,                  /* Height of the window in pixels */
-                              0);                      /* Additional flag(s) */
-
-    /* Checks if window has been created; if not, exits program */
-    if (window == NULL)
-    {
-        std::cerr << "SDL window failed to initialise: " << SDL_GetError() << '\n';
-        return 1;
-    }
-
-    std::array<std::uint8_t, WIDTH * 4 * HEIGHT> DISPLAY {};
-    SDL_Renderer*                                renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE | SDL_RENDERER_PRESENTVSYNC);
-    if (renderer == NULL)
-    {
-        std::cerr << "SDL renderer failed to initialise: " << SDL_GetError() << '\n';
-        return 1;
-    }
-
-    SDL_Surface* surf = SDL_CreateRGBSurfaceFrom(DISPLAY.data(), WIDTH, HEIGHT, 32, 4 * WIDTH, 0xFF000000, 0x00FF0000, 0x0000FF00, 0x000000FF);
-    if (surf == NULL)
-    {
-        std::cerr << "SDL surface cration failed: " << SDL_GetError() << '\n';
-        return 1;
-    }
-
-    SDL_Texture* texTarget = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, WIDTH, HEIGHT);
-    if (texTarget == NULL)
-    {
-        std::cerr << "SDL could not create texturetarget: " << SDL_GetError() << '\n';
-        return 1;
-    }
 
     std::array<std::uint8_t, MAX_ADDR> RAM {};
     std::array<std::uint8_t, 0x10>     V {};
-    std::array<std::uint16_t, 0x10>    STACK {};
+    //    std::array<std::uint16_t, 0x10>    STACK {};
     // consider making the rgb to point on a single byte?
-    std::uint8_t  delay {}, sound {}, SP {};
+    // std::uint8_t  delay {}, sound {}, SP {};
     std::uint16_t PC {}, I {};
 
     const std::uint16_t program_base = 0x200;
@@ -104,10 +151,6 @@ int main(int argc, char** argv)
     std::cout << "Total: " << std::dec << program_end - program_base + 1 << " bytes" << std::endl;
 
     PC = program_base;
-    for (std::size_t i = 0; i < DISPLAY.size(); ++i)
-    {
-        DISPLAY[i] = 0xFF;
-    }
 
     while (PC <= program_end)
     {
@@ -122,14 +165,7 @@ int main(int argc, char** argv)
             {
             case 0xE0: // clear
             {
-                for (std::size_t i = 0; i < HEIGHT; ++i)
-                {
-                    for (std::size_t j = 0; j < WIDTH; ++j)
-                    {
-                        DISPLAY[i * WIDTH * 4 + j * 4 + 2] = DISPLAY[i * WIDTH * 4 + j * 4 + 1] = DISPLAY[i * WIDTH * 4 + j * 4] = 0x00;
-                    }
-                }
-                // TODO("Clear the display");
+                display.reset(0);
             }
             break;
             case 0xEE:
@@ -162,19 +198,14 @@ int main(int argc, char** argv)
             std::uint8_t       x = V[(operation >> 8) & 0x0F] % WIDTH;
             std::uint8_t       y = V[(operation >> 4) & 0x0F] % HEIGHT;
             const std::uint8_t n = operation & 0x0F; // sprite height
-
             for (std::uint8_t i = 0; i < n && (y + i) < HEIGHT; ++i)
             {
                 // check if out of bounds?
                 std::uint8_t sprite_row = RAM[I + i];
-                for (std::uint8_t j = 0; j < 8u && (x + j) < WIDTH; ++j)
-                {
-                    const std::uint8_t pixel_value                 = ((sprite_row >> (7 - j)) & 0x1) * 255;
-                    DISPLAY[(y + i) * WIDTH * 4 + (x + j) * 4 + 2] = DISPLAY[(y + i) * WIDTH * 4 + (x + j) * 4 + 1] = DISPLAY[(y + i) * WIDTH * 4 + (x + j) * 4] ^= pixel_value;
-                }
+                display.set_pixels(x, y + i, sprite_row);
             }
+            display.draw();
             std::cout << (int)x << " " << (int)y << " " << (int)n << std::endl;
-            // TODO("Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision");
             break;
         }
         case 0x1:
@@ -203,42 +234,43 @@ int main(int argc, char** argv)
             {
             case SDL_QUIT:
                 SDL_Log("Quite event detected!");
-                goto END;
+                return 0;
+                break;
+            case SDL_WINDOWEVENT:
+            {
+                if (e.window.event == SDL_WINDOWEVENT_RESIZED || e.window.event == SDL_WINDOWEVENT_RESIZED)
+                    display.draw();
+                break;
+            }
+            default:
                 break;
             }
         }
-
-        SDL_Texture* tex = SDL_CreateTextureFromSurface(renderer, surf);
-        if (tex == NULL)
-        {
-            std::cerr << "SDL could not create texture: " << SDL_GetError() << '\n';
-            return 1;
-        }
-        SDL_RenderClear(renderer);
-
-        SDL_SetRenderTarget(renderer, texTarget);
-        SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, tex, NULL, NULL);
-
-        SDL_SetRenderTarget(renderer, NULL);
-        // SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, texTarget, NULL, NULL);
-        SDL_RenderPresent(renderer);
-
-        SDL_DestroyTexture(tex);
+        // display.draw();
         if (inc_pc)
             PC += 2;
     }
 
-END:
-    /* Frees memory */
-    SDL_FreeSurface(surf);
-    SDL_DestroyTexture(texTarget);
-    // SDL_DestroyTexture(tex);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+    return 0;
+}
 
+int main(int argc, char** argv)
+{
+    static_cast<void>(argc);
+    static_cast<void>(argv);
+
+    /*
+     * Initialises the SDL video subsystem (as well as the events subsystem).
+     * Returns 0 on success or a negative error code on failure using SDL_GetError().
+     */
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        std::cerr << "SDL failed to initialise: " << SDL_GetError() << '\n';
+        return 1;
+    }
+
+    int ret = run();
     /* Shuts down all SDL subsystems */
     SDL_Quit();
-    return 0;
+    return ret;
 }
